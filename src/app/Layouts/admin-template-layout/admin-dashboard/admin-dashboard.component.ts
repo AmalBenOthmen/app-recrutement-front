eff9ba09401435507cb6bc30949a7f07ace633a5
amal <amalbenothmen9@gmail.com>
2024-07-19 18:37:44 +0100

    notification was displayed to admin when new message has received

--- src/app/Layouts/admin-template-layout/admin-dashboard/admin-dashboard.component.ts
+++ src/app/Layouts/admin-template-layout/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule, RouterOutlet} from "@angular/router";
+import {Message} from "../../../services/models/message";
+import {MessageService} from "../../../services/services/messageService";
 
 
 @Component({
@@ -15,6 +17,25 @@ import {RouterModule, RouterOutlet} from "@angular/router";
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  messages: Message[] = [];
+  public unreadCount: number=0;
 
-}
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messageService.messages$.subscribe(messages => {
+      this.messages = messages.reverse();
+      this.unreadCount = this.messages.filter(message => !message.read).length;
+    });
+
+    this.messageService.loadMessages();
+  }
+
+  markAllAsRead() {
+    this.messages.forEach(message => {
+      if (!message.read) {
+        this.messageService.markAsRead(message.id!).subscribe();
+      }
+    });
+  }}
--- src/app/services/services/messageService.ts
+++ src/app/services/services/messageService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { TokenService } from '../token/token.service';
 import { Message } from '../models/message'; // Adjust path based on your project structure
 
@@ -9,15 +10,38 @@ import { Message } from '../models/message'; // Adjust path based on your projec
 })
 export class MessageService {
   private apiUrl = 'http://localhost:8089/api/v1'; // Replace with your backend base URL
+  private messagesSubject = new BehaviorSubject<Message[]>([]);
+  messages$ = this.messagesSubject.asObservable();
+  private ws!: WebSocket;
 
-  constructor(private http: HttpClient, private tokenService: TokenService) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {
+    this.connectWebSocket();
+  }
+
+  connectWebSocket() {
+    this.ws = new WebSocket('ws://localhost:8089/ws'); // Replace with your WebSocket URL
+
+    this.ws.onmessage = (event) => {
+      const newMessage: Message = JSON.parse(event.data);
+      const messages = this.messagesSubject.getValue();
+      messages.push(newMessage);
+      this.messagesSubject.next([...messages]);
+    };
+
+    this.ws.onclose = () => {
+      console.log('WebSocket connection closed. Reconnecting...');
+      setTimeout(() => this.connectWebSocket(), 1000);
+    };
+  }
 
   saveMessage(message: Message): Observable<Message> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${this.tokenService.getToken()}`
     });
-    return this.http.post<Message>(`${this.apiUrl}/messages/send`, message, { headers });
+    return this.http.post<Message>(`${this.apiUrl}/messages/send`, message, { headers }).pipe(
+      tap(() => this.loadMessages()) // Reload messages after saving a new one
+    );
   }
 
   getUnreadMessages(): Observable<Message[]> {
@@ -33,7 +57,16 @@ export class MessageService {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${this.tokenService.getToken()}`
     });
-    return this.http.put<Message>(`${this.apiUrl}/messages/read/${id}`, {}, { headers });
+    return this.http.put<Message>(`${this.apiUrl}/messages/read/${id}`, {}, { headers }).pipe(
+      tap(() => {
+        const messages = this.messagesSubject.getValue();
+        const messageIndex = messages.findIndex(m => m.id === id);
+        if (messageIndex !== -1) {
+          messages[messageIndex].read = true;
+          this.messagesSubject.next([...messages]);
+        }
+      })
+    );
   }
 
   getAllMessages(): Observable<Message[]> {
@@ -41,6 +74,12 @@ export class MessageService {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${this.tokenService.getToken()}`
     });
-    return this.http.get<Message[]>(`${this.apiUrl}/messages/all`, { headers });
+    return this.http.get<Message[]>(`${this.apiUrl}/messages/all`, { headers }).pipe(
+      tap(messages => this.messagesSubject.next(messages))
+    );
+  }
+
+  loadMessages() {
+    this.getAllMessages().subscribe();
   }
 }
